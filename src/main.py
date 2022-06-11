991660213dd3b6b584dd037d151b67f5533a9b2c
Edward Chamberlain <edward.chamberlain@me.com>
2022-06-12 00:03:14 +0100

    update graph size for trinket

--- src/main.py
+++ src/main.py
@@ -64,8 +64,9 @@ my_camera = planar_projection.Camera_3D(
     projection_plane_distance=-10
 )
 
+GRAPH_SIZE = (350,350) if sg.running_trinket() else (500,500)
 layout = [
-    [sg.Graph((500, 500), (-1, -1), (1, 1), 'black', float_values=True, enable_events=True, key='-GRAPH-', drag_submits=True)],
+    [sg.Graph(GRAPH_SIZE, (-1, -1), (1, 1), 'black', float_values=True, enable_events=True, key='-GRAPH-', drag_submits=True)],
     [sg.Text("R:", size=3), sg.Slider((0, 360), resolution=1, default_value=0, enable_events=True, orientation='horizontal', expand_x=True, key='-O-')],
     [sg.Text("X:", size=3), sg.Slider((-8, 8), resolution=0.1, default_value=0, enable_events=True, orientation='horizontal', expand_x=True, key='-X-')],
     [sg.Text("Y:", size=3), sg.Slider((-3, 3), resolution=0.1, default_value=0, enable_events=True, orientation='horizontal', expand_x=True, key='-Y-')],
